e07a0963926497e92a43f4248d03fd6b86790be4
John Pearson <grendeloz@gmail.com>
2022-04-22 18:32:39 +1000

    Added SubSequence method. Added tests for NewSequence and SubSequence.

--- genome/sequence.go
+++ genome/sequence.go
@@ -1,6 +1,7 @@
 package genome
 
 import (
+	"fmt"
 	"log"
 	"regexp"
 	"strings"
@@ -53,6 +54,44 @@ func NewSequence(header string) *Sequence {
 	return seq
 }
 
+// Length is the length of the sequence.
 func (s *Sequence) Length() int {
 	return len(s.Sequence)
+}
+
+// SubSequence returns a subsequence of Sequence.Sequence. The start and
+// end values form a 1-based closed interval, i.e. the start and end
+// bases are both included in the subsequence. Setting end to 0 is a
+// special case - it signals that you want all sequence from the start
+// position to the end of the sequence.
+//
+// Given a sequence ACGTTGCA, this list shows what would be returned if
+// we were to call SubSequence with the start,end values:
+//
+//   1,4 : ACGT - 4-base sequence from the 1st to 4th base inclusive.
+//   0,4 : error, start cannot be less than 1
+//   1,8 : ACGTTGCA - returns the entire sequence
+//   1,0 : ACGTTGCA - returns the entire sequence, special case
+//   1,9 : error - end cannot be > length(sequence)
+//   4,3 : error - start cannot be > end
+//   5,5 : T - returns a single base
+func (s *Sequence) SubSequence(start, end int) (string, error) {
+	// Note that golang substrings are 0-based half-open intervals, i.e.
+	// the start is included but the end is the first base past the end of
+	// the subsequence. So 1,4 returns the 2nd and 3rd bases. It is on
+	// us to do the conversion.
+	switch {
+	case start < 1:
+		return "", fmt.Errorf("SubSequence: start cannot be less than 1: %d", start)
+	case end > len(s.Sequence):
+		return "", fmt.Errorf("SubSequence: end cannot be beyond the end of the sequence: %d", end)
+	case start > len(s.Sequence):
+		return "", fmt.Errorf("SubSequence: start cannot be beyond the end of the sequence: %d", start)
+	case end == 0:
+		// must come before start>end case
+		end = len(s.Sequence)
+	case start > end:
+		return "", fmt.Errorf("SubSequence: start cannot be > end: %d", start)
+	}
+	return s.Sequence[start-1 : end], nil
 }
--- genome/sequence_test.go
+++ genome/sequence_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestSequence(t *testing.T) {
+func TestSequenceFromGenome(t *testing.T) {
 	name := "testing"
 	genome := NewGenome(name)
 
@@ -54,5 +54,88 @@ func TestSequence(t *testing.T) {
 	g10 := seq2.Name
 	if e10 != g10 {
 		t.Fatalf(`seq Name incorrect - should be %v but is %v`, e10, g10)
+	}
+}
+
+func TestNewSequence(t *testing.T) {
+	h1 := `>chrJP | my test seq`
+	s1 := NewSequence(h1)
+
+	e1 := h1
+	g1 := s1.Header
+	if e1 != g1 {
+		t.Fatalf(`Sequence Header incorrect, should be %v but is %v`, e1, g1)
+	}
+
+	e2 := `chrJP`
+	g2 := s1.Name
+	if e2 != g2 {
+		t.Fatalf(`Sequence Name incorrect, should be %v but is %v`, e2, g2)
+	}
+
+	e3 := `my test seq`
+	g3 := s1.Info
+	if e3 != g3 {
+		t.Fatalf(`Sequence Info incorrect, should be %v but is %v`, e3, g3)
+	}
+}
+
+func TestSubSequence(t *testing.T) {
+	h1 := `>chrJP | my test seq`
+	s1 := NewSequence(h1)
+	s1.Sequence = `ACGTTGCA`
+
+	e1 := s1.Sequence
+	g1, err := s1.SubSequence(1, 0)
+	if err != nil {
+		t.Fatalf(`SubSequence 1,0 should have worked but failed: %v`, err)
+	}
+	if e1 != g1 {
+		t.Fatalf(`SubSequence special case failed - should be %v but is %v`, e1, g1)
+	}
+
+	e2 := s1.Sequence
+	g2, err := s1.SubSequence(1, 8)
+	if err != nil {
+		t.Fatalf(`SubSequence 1,8 should have worked but failed: %v`, err)
+	}
+	if e2 != g2 {
+		t.Fatalf(`SubSequence 1,8 failed - should be %v but is %v`, e2, g2)
+	}
+
+	e3 := `CGTT`
+	g3, err := s1.SubSequence(2, 5)
+	if err != nil {
+		t.Fatalf(`SubSequence 2,5 should have worked but failed: %v`, err)
+	}
+	if e3 != g3 {
+		t.Fatalf(`SubSequence 2,5 failed - should be %v but is %v`, e3, g3)
+	}
+
+	e4 := `AC`
+	g4, err := s1.SubSequence(1, 2)
+	if err != nil {
+		t.Fatalf(`SubSequence 1,2 should have worked but failed: %v`, err)
+	}
+	if e4 != g4 {
+		t.Fatalf(`SubSequence 2,5 failed - should be %v but is %v`, e4, g4)
+	}
+
+	// Test the various error modes.
+	g5, err := s1.SubSequence(0, 2)
+	if err == nil {
+		t.Fatalf(`SubSequence 0,2 should have failed but instead returned: %v`, g5)
+	}
+	g6, err := s1.SubSequence(2, 10)
+	if err == nil {
+		t.Fatalf(`SubSequence 2,10 should have failed but instead returned: %v`, g6)
+	}
+	g7, err := s1.SubSequence(7, 6)
+	if err == nil {
+		t.Fatalf(`SubSequence 7,6 should have failed but instead returned: %v`, g7)
+	}
+	g8, err := s1.SubSequence(9, 10)
+	if err == nil {
+		t.Fatalf(`SubSequence 9,10 should have failed but instead returned: %v`, g8)
 	}
 }
